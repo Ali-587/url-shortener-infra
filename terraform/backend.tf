3765db6fb47a295bb2023dda0f8aaa13fd92ea57
Ali-587 <alimcm587@gmail.com>
2026-05-15 16:08:32 +0200

    Second commit of day

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "url-shortener-terraform-state-bucket"
-    key            = "url-shortener/dev/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
+    bucket       = "url-shortener-terraform-state-bucket"
+    key          = "url-shortener/dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
