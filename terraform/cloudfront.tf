7ab986a057406e9f0a323e326058e7fd86e4be51
Ali-587 <alimcm587@gmail.com>
2026-05-15 22:56:24 +0200

    Commiting updated infra

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -26,10 +26,11 @@ resource "aws_cloudfront_cache_policy" "api_cache_disabled" {
     }
 
     query_strings_config {
-      query_string_behavior = "all"
+      query_string_behavior = "none"
     }
   }
 }
+
 
 resource "aws_cloudfront_origin_request_policy" "api_origin_request_policy" {
   name    = "${local.name}-api-origin-request-policy"
